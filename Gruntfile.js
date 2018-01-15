f121ec8bf92585f29bf28ad68c880a551d0d26bc
David Miller <davidfmiller@me.com>
2018-01-15 09:57:07 -0800

    update css build path

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,7 +24,7 @@ module.exports = function(grunt) {
       dist: {
         options: {
           sassDir: 'src/styles',
-          cssDir: 'docs/assets/css',
+          cssDir: 'docs/build',
           environment: 'production',
           outputStyle: 'compressed'
         }
