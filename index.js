0e84eb31049c0c2708831e8a48d333f7d52ec617
davidfmiller <davidfmiller@me.com>
2019-02-22 11:13:54 -0700

    cleanup

--- index.js
+++ index.js
@@ -147,35 +147,6 @@
     return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(str);
   },
 
-/*
-- (NSString*) longitudeString
-{
-    double value = self.longitude;
-    NSString *dir = value < 0 ? @"W" : @"E";
-
-    int degrees = (int) value;
-    value = (value - degrees) * 60;
-
-    int minutes = (int) value;
-    float seconds = (value - minutes) * 60;
-
-    return [NSString stringWithFormat:@"%dº%0d’%2.1f”%@", abs(degrees), abs(minutes), fabs(seconds), dir, nil];
-}
-
-- (NSString*) latitudeString
-{
-    double value = self.latitude;
-    NSString *dir = value < 0 ? @"S" : @"N";
-
-    int degrees = (int) value;
-    value = (value - degrees) * 60;
-
-    int minutes = (int) value;
-    float seconds = (value - minutes) * 60;
-
-    return [NSString stringWithFormat:@"%dº%02d’%2.1f”%@", abs(degrees), abs(minutes), fabs(seconds), dir, nil];
-}
-*/
 
   /**
     Format a latitude coordinate value into a human-friendly string
