074a75e1b5f7c4657c85a9db8399e7b3ec1392fe
David Miller <david.miller@showpass.com>
2020-01-30 14:37:23 -0700

    breakpoints

--- index.js
+++ index.js
@@ -1229,7 +1229,14 @@
       isSafari: isSafari,
       isFirefox: isFirefox,
       scrollTo: scrollTo,
-      opensData: opensData
+      opensData: opensData,
+      breakPoints: {
+        xs: 0,
+        sm: 576,
+        md: 768,
+        lg: 992,
+        xl: 1200
+      }
     },
     String: {
       isURL: isURL,
